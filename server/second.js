d411d4779ff4c6d835559ed100f549d1aa86171e
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-14 09:08:42 +0530

    first commit

---
+++ server/second.js
@@ -0,0 +1,13 @@
+
+const fun =function(){
+    console.log("Javascript is tricky")
+}
+
+const people = ["Ram","Rajan","Naveen"]
+
+const test = {sno:1, name: "John"}
+
+module.exports = {
+    message: fun, people :people,  person: test
+}
+
---
+++ server/third.js
@@ -0,0 +1,2 @@
+module.exports.funny = "India is in Asia";
+module.exports.test =function(){ return " INDIA "}
