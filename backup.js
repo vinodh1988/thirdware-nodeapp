fcf6ea40f1d0541a49a16e252c4df9bb8ee9683b
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-14 09:40:28 +0530

    server creation

---
+++ backup.js
@@ -0,0 +1,17 @@
+const obj2 = require('./server/third')
+
+obj.message();
+console.log(obj.people)
+console.log(obj.person)
+console.log(obj2.test())
+console.log(obj2.funny);
+
+
+fs.readFile("package.json","utf-8",function(error,data){
+    if(error)
+       console.log("file not read")
+    else
+        console.log(data);
+})
+
+console.log("After reading file logic")
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
 const obj = require('./server/second')
-const obj2 = require('./server/third')
-
-obj.message();
-console.log(obj.people)
-console.log(obj.person)
-console.log(obj2.test())
-console.log(obj2.funny);
+const express=require("express");
+const app = express();
 
 
-fs.readFile("package.json","utf-8",function(error,data){
-    if(error)
-       console.log("file not read")
-    else
-        console.log(data);
+app.get("/",function(request,response){
+
+    response.send("Node JS server is running...!!!")
 })
 
-console.log("After reading file logic")
+app.listen("4800",function(){
+    console.log("Server started with port number 4800")
+})
