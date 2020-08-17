a11f7207445e47cc82b53ef9c702cc6922dc30dc
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-17 11:30:50 +0530

    Async Syntax

--- server/routes/people/org.js
+++ server/routes/people/org.js
@@ -3,18 +3,35 @@ const router = express.Router();
 const Department = require('../../model/departments');
 
 
-router.get("/departments",function(request,response){
-    Department.query().eager('employees').then(
+router.get("/departments",async function(request,response){
+   /* Department.query().withGraphFetched('employees').then(
         (data)=>response.json(data),
         ()=>response.json([])
-    )
+    )*/
+
+    try{
+        let data = await Department.query().withGraphFetched('employees'); 
+        response.json(data) ;
+    }
+    catch(e){
+        console.log(e);
+        response.json([]);
+    }
 })
 
-router.post("/departments",function(request,response){
-     Department.query().insert(request.body).then(
+router.post("/departments",async function(request,response){
+   /*  Department.query().withGraphFetched('employees').insertWithRelated(request.body).then(
          ()=>response.send({status:"Success"}),
          ()=>response.status(500)
-     )
+     )*/
+     try{
+         await Department.query().insertWithRelated(request.body)
+         response.send({status:"success"})
+     }
+     catch(e){
+         console.log(e);
+         response.status(500)
+     }
    
 })
 
