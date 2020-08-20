ee32ffc21d035b295de19315bc56a21a212ac4d1
vinodh1988 <k.c.vinodh@hotmail.com>
2020-08-20 11:39:54 +0530

    people post commit

--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -1,6 +1,7 @@
 const offices =require ("../../mongodb/schemas/offices");
 const people =require ("../../mongodb/schemas/people");
-const express = require('express')
+const express = require('express');
+const mongoose = require("mongoose");
 const router = express.Router();
 
 router.get("/offices",(request,response)=>{
@@ -14,12 +15,31 @@ router.get("/offices",(request,response)=>{
 
 router.get("/people",(request,response)=>{
     people.find({},{_id:0},(error,data)=>{
+        
         if(error)
            response.status(500);
         else
-           response.json(data);
+           response.json({"people": data});
     })
 })
+
+router.post("/people",(request,response)=>{
+    people.aggregate([{$group: {_id:"max",max:{$max:"$id"}}}],function(err,data){
+        
+       let id=data[0].max+1;
+       let obj ={...request.body.person,id: id};
+       console.log(obj);
+       people.insertMany(obj,(error,data)=>{
+        
+        if(error)
+           response.status(500);
+        else
+           response.json( {"person": {id:id}});
+    })
+})
+})
+
+
 
 
 
